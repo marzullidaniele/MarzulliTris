1fd2f7bacb95fe6422f91e7db968aed06f9ca09b
mars <marzulli.daniele@gmail.com>
2018-02-26 12:48:09 +0100

    ciao

--- app/src/main/java/com/example/mars/marzullitris/AvversarioPersona.java
+++ app/src/main/java/com/example/mars/marzullitris/AvversarioPersona.java
@@ -5,19 +5,30 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AvversarioPersona extends AppCompatActivity {
     Button b;
+    EditText g1;
+    EditText g2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.avversario);
         b=findViewById(R.id.button4);
+        g1=findViewById(R.id.editText);
+        g2=findViewById(R.id.editText2);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(AvversarioPersona.this, Partita.class);
-                startActivity(i);
+                if(!g1.getText().equals("") && !g2.getText().equals("")){
+                    Intent i = new Intent(AvversarioPersona.this, Partita.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Compilare tutti i campi!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
