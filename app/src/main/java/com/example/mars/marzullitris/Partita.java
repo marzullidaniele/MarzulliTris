067cbe50182434490a5f4ffc01878ab94eb151ee
studente <ciaooo@gmail.com>
2018-03-08 10:57:31 +0100

    ciao

--- app/src/main/java/com/example/mars/marzullitris/Partita.java
+++ app/src/main/java/com/example/mars/marzullitris/Partita.java
@@ -13,13 +13,17 @@ public class Partita  extends AppCompatActivity {
     TextView tv2;
     TextView tv3;
     Button b1;
+    Button b2;
+    int conta=0;
+    String n1;
+    String n2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.partita);
         Bundle nomi = getIntent().getExtras();
-        String n1=nomi.getString("n1");
-        String n2 =nomi.getString("n2");
+        n1=nomi.getString("n1");
+        n2 =nomi.getString("n2");
         tv1=findViewById(R.id.textView5);
         tv1.setText(n1 + "->");
         tv2=findViewById(R.id.textView6);
@@ -27,10 +31,35 @@ public class Partita  extends AppCompatActivity {
         tv3=findViewById(R.id.textView10);
         tv3.setText(n1);
         b1=findViewById(R.id.imageButton);
+        b2=findViewById(R.id.imageButton1);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b1.setBackground(getDrawable(R.drawable.x));
+                if (conta % 2 == 0) {
+                    b1.setBackground(getDrawable(R.drawable.x));
+                    b1.setClickable(false);
+                    conta++;
+                } else {
+                    b1.setBackground(getDrawable(R.drawable.cerchio));
+                    b1.setClickable(false);
+                    conta++;
+                }
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conta % 2 == 0) {
+                    b2.setBackground(getDrawable(R.drawable.x));
+                    b2.setClickable(false);
+                    tv3.setText(n2);
+                    conta++;
+                } else {
+                    b2.setBackground(getDrawable(R.drawable.cerchio));
+                    b2.setClickable(false);
+                    tv3.setText(n1);
+                    conta++;
+                }
             }
         });
     }
