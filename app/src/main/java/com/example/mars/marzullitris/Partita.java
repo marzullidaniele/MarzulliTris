0656ec2d2eea56f9165ab5254dff21f5e8a8cbcc
studente <marz@gmail.com>
2018-03-10 10:33:14 +0100

    ciao

--- app/src/main/java/com/example/mars/marzullitris/Partita.java
+++ app/src/main/java/com/example/mars/marzullitris/Partita.java
@@ -14,6 +14,13 @@ public class Partita  extends AppCompatActivity {
     TextView tv3;
     Button b1;
     Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    Button b6;
+    Button b7;
+    Button b8;
+    Button b9;
     int conta=0;
     String n1;
     String n2;
@@ -32,16 +39,25 @@ public class Partita  extends AppCompatActivity {
         tv3.setText(n1);
         b1=findViewById(R.id.imageButton);
         b2=findViewById(R.id.imageButton1);
+        b3=findViewById(R.id.imageButton2);
+        b4=findViewById(R.id.imageButton3);
+        b5=findViewById(R.id.imageButton4);
+        b6=findViewById(R.id.imageButton5);
+        b7=findViewById(R.id.imageButton6);
+        b8=findViewById(R.id.imageButton7);
+        b9=findViewById(R.id.imageButton8);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (conta % 2 == 0) {
                     b1.setBackground(getDrawable(R.drawable.x));
                     b1.setClickable(false);
+                    tv3.setText(n2);
                     conta++;
                 } else {
                     b1.setBackground(getDrawable(R.drawable.cerchio));
                     b1.setClickable(false);
+                    tv3.setText(n1);
                     conta++;
                 }
             }
@@ -57,6 +73,118 @@ public class Partita  extends AppCompatActivity {
                 } else {
                     b2.setBackground(getDrawable(R.drawable.cerchio));
                     b2.setClickable(false);
+                    tv3.setText(n1);
+                    conta++;
+                }
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conta % 2 == 0) {
+                    b3.setBackground(getDrawable(R.drawable.x));
+                    b3.setClickable(false);
+                    tv3.setText(n2);
+                    conta++;
+                } else {
+                    b3.setBackground(getDrawable(R.drawable.cerchio));
+                    b3.setClickable(false);
+                    tv3.setText(n1);
+                    conta++;
+                }
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conta % 2 == 0) {
+                    b4.setBackground(getDrawable(R.drawable.x));
+                    b4.setClickable(false);
+                    tv3.setText(n2);
+                    conta++;
+                } else {
+                    b4.setBackground(getDrawable(R.drawable.cerchio));
+                    b4.setClickable(false);
+                    tv3.setText(n1);
+                    conta++;
+                }
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conta % 2 == 0) {
+                    b5.setBackground(getDrawable(R.drawable.x));
+                    b5.setClickable(false);
+                    tv3.setText(n2);
+                    conta++;
+                } else {
+                    b5.setBackground(getDrawable(R.drawable.cerchio));
+                    b5.setClickable(false);
+                    tv3.setText(n1);
+                    conta++;
+                }
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conta % 2 == 0) {
+                    b6.setBackground(getDrawable(R.drawable.x));
+                    b6.setClickable(false);
+                    tv3.setText(n2);
+                    conta++;
+                } else {
+                    b6.setBackground(getDrawable(R.drawable.cerchio));
+                    b6.setClickable(false);
+                    tv3.setText(n1);
+                    conta++;
+                }
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conta % 2 == 0) {
+                    b7.setBackground(getDrawable(R.drawable.x));
+                    b7.setClickable(false);
+                    tv3.setText(n2);
+                    conta++;
+                } else {
+                    b7.setBackground(getDrawable(R.drawable.cerchio));
+                    b7.setClickable(false);
+                    tv3.setText(n1);
+                    conta++;
+                }
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conta % 2 == 0) {
+                    b8.setBackground(getDrawable(R.drawable.x));
+                    b8.setClickable(false);
+                    tv3.setText(n2);
+                    conta++;
+                } else {
+                    b8.setBackground(getDrawable(R.drawable.cerchio));
+                    b8.setClickable(false);
+                    tv3.setText(n1);
+                    conta++;
+                }
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (conta % 2 == 0) {
+                    b9.setBackground(getDrawable(R.drawable.x));
+                    b9.setClickable(false);
+                    tv3.setText(n2);
+                    conta++;
+                } else {
+                    b9.setBackground(getDrawable(R.drawable.cerchio));
+                    b9.setClickable(false);
                     tv3.setText(n1);
                     conta++;
                 }
