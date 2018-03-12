b77775d4f0c7dd7fabb6efdae456c9ca0cb40aad
studente <ciao@gmail.com>
2018-03-12 12:48:06 +0100

    ciao

--- app/src/main/java/com/example/mars/marzullitris/MainActivity.java
+++ app/src/main/java/com/example/mars/marzullitris/MainActivity.java
@@ -5,10 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1;
+    Button b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, AvversarioPersona.class);
                 startActivity(i);
+            }
+        });
+        b2=findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Questo non è un pc!!!", Toast.LENGTH_LONG).show();
             }
         });
     }
